69b4cdf042855c04ba8f200882c9bf2aaf5c6789
nafi sarkar <sarkarnafe@gamil.com>
2025-01-13 13:26:01 +0600

    update backend

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,21 @@ import dotenv from "dotenv";
 import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import cors from "cors";
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
+
+// Enable CORS
+app.use(cors(
+  {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  }
+));
 const __dirname = path.resolve();
 
 // Middleware to parse JSON data
