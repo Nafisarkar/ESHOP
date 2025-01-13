4a7c6441f2acfe8a30a3643aaf37f308a4817ca3
nafi sarkar <sarkarnafe@gamil.com>
2025-01-13 13:20:05 +0600

    hot fix

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -7,7 +7,7 @@ export const useProductStore = create((set) => ({
     if (!newProduct.name || !newProduct.price || !newProduct.image) {
       return { success: false, message: "All fields are required" };
     }
-    const res = await fetch("/api/products", {
+    const res = await fetch("https://eshop-gilt-chi.vercel.app/api/products", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -19,12 +19,12 @@ export const useProductStore = create((set) => ({
     return { success: true, message: "Product created successfully" };
   },
   fetchProducts: async () => {
-    const res = await fetch("/api/products");
+    const res = await fetch("https://eshop-gilt-chi.vercel.app/api/products");
     const data = await res.json();
     set({ products: data.data });
   },
   deleteProduct: async (pid) => {
-    const res = await fetch(`/api/products/${pid}`, {
+    const res = await fetch(`https://eshop-gilt-chi.vercel.app/api/products/${pid}`, {
       method: "DELETE",
     });
     const data = await res.json();
@@ -37,7 +37,7 @@ export const useProductStore = create((set) => ({
     return { success: true, message: "Product deleted successfully" };
   },
   updateProduct: async (pid, updatedProduct) => {
-    const res = await fetch(`/api/products/${pid}`, {
+    const res = await fetch(`https://eshop-gilt-chi.vercel.app/api/products/${pid}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
