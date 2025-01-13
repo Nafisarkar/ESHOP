423dd20547c5c49c4bce44c7c93c81f4a20dc9c6
nafi sarkar <sarkarnafe@gamil.com>
2025-01-13 12:23:39 +0600

    update server js

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,6 @@ app.use(express.json()); // to accept json data in the body of the request
 
 app.use("/api/products",productRoutes)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) =>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")); 
-  })
-}
 
 app.listen(PORT, () => {
   connectDB();
